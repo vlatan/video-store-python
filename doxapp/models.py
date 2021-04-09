9d1cfaeef3f47928190be5518b1c9cc173993c47
vlatan <33163699+vlatan@users.noreply.github.com>
2021-04-10 00:40:12 +0200

    Modify Post model

--- doxapp/models.py
+++ doxapp/models.py
@@ -11,7 +11,9 @@ def load_user(user_id):
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     openid = db.Column(db.String(256), unique=True, nullable=False)
-    email = db.Column(db.String(256), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(120), nullable=False)
+    picture = db.Column(db.String(256), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
 
     # def __repr__(self):
@@ -20,10 +22,12 @@ class User(db.Model, UserMixin):
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    provider = db.Column(db.String(7), nullable=False)
+    video_id = db.Column(db.String(20), nullable=False)
     title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False,
                             default=datetime.utcnow)
-    content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey(User.id), nullable=False)
 
     # def __repr__(self):
