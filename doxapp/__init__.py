e4cd54a52c3d291d117d9fd2a5d3551c4bffa20c
vlatan <33163699+vlatan@users.noreply.github.com>
2021-11-16 13:38:33 +0100

    Recreate db

--- doxapp/__init__.py
+++ doxapp/__init__.py
@@ -29,7 +29,7 @@ def create_app(config_class=Config):
     app.register_blueprint(errors)
 
     # when you want to create the tables for the first time
-    # with app.app_context():
-    #     db.create_all()
+    with app.app_context():
+        db.create_all()
 
     return app
