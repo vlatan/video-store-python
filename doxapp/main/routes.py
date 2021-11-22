8df54503022a156d2d922bd2f2d2664daca793e4
vlatan <33163699+vlatan@users.noreply.github.com>
2021-11-22 21:42:45 +0100

    Don't call the backend twice on the first visit

--- doxapp/main/routes.py
+++ doxapp/main/routes.py
@@ -5,39 +5,32 @@ from doxapp.models import Post
 main = Blueprint('main', __name__)
 
 
-# @main.route('/')
-# # @main.route('/home/')
-# def home():
-#     page = request.args.get('page', 1, type=int)
-#     posts = Post.query.order_by(
-#         Post.date_posted.desc()).paginate(page=page, per_page=10)
-#     return render_template('home.html', posts=posts)
-
 @main.route('/')
 def home():
-    return render_template('home.html')
-
-
-@main.route('/load')
-def load():
     """ Route to return the posts """
-    time.sleep(0.2)  # simulate delay
+
     quantity = 12    # number of posts to fetch per load
+    # Query the Post table by descending date
+    posts = Post.query.order_by(Post.date_posted.desc())
 
-    if request.args:
-        # The 'counter' value sent in the query string
+    # If there's a query string in the request
+    if request.args.get("c"):
+        # Get the 'counter' value sent in the query string
         counter = int(request.args.get("c"))
-
-        # Query the Post table by descending date
-        posts = Post.query.order_by(Post.date_posted.desc())
         # Get a coresponding slice of the query
         posts = posts.slice(counter, counter + quantity)
         # Serialize and jsonify the posts to be read by JavaScript
         posts = jsonify([post.serialize for post in posts])
 
+        time.sleep(0.2)     # Simulate delay
+
         # print(f"Returning posts {counter} to {counter + quantity}")
-        # Slice counter -> quantity from the db
         return make_response(posts, 200)
+
+    # Get posts for the first load
+    posts = posts.limit(quantity)
+
+    return render_template('home.html', posts=posts, quantity=quantity)
 
 
 @main.route('/about/')
