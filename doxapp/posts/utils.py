b3a0926125dab45a6b4a9e42ffdb72143331d5fa
vlatan <33163699+vlatan@users.noreply.github.com>
2021-11-27 18:43:43 +0100

    Add scheduling Class

--- doxapp/posts/utils.py
+++ doxapp/posts/utils.py
@@ -6,6 +6,34 @@ from urllib.parse import urlparse, parse_qs, quote_plus
 from googleapiclient.discovery import build
 from flask import current_app
 from wtforms.validators import ValidationError
+from threading import Timer
+
+
+# https://stackoverflow.com/questions/2398661/schedule-a-repeating-event-in-python-3
+class RepeatedTimer(object):
+    def __init__(self, interval, function, *args, **kwargs):
+        self._timer = None
+        self.function = function
+        self.interval = interval
+        self.args = args
+        self.kwargs = kwargs
+        self.is_running = False
+        self.start()
+
+    def _run(self):
+        self.is_running = False
+        self.start()
+        self.function(*self.args, **self.kwargs)
+
+    def start(self):
+        if not self.is_running:
+            self._timer = Timer(self.interval, self._run)
+            self._timer.start()
+            self.is_running = True
+
+    def stop(self):
+        self._timer.cancel()
+        self.is_running = False
 
 
 def extract_video_id(url):
