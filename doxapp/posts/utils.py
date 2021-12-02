bbb16e79ed4c5978181ae162f154a603f4a9a055
vlatan <33163699+vlatan@users.noreply.github.com>
2021-12-02 18:55:25 +0100

    Move class

--- doxapp/posts/utils.py
+++ doxapp/posts/utils.py
@@ -6,42 +6,7 @@ from urllib.parse import urlparse, parse_qs, quote_plus
 from googleapiclient.discovery import build
 from flask import current_app
 from wtforms.validators import ValidationError
-from threading import Timer, Lock
 from doxapp.models import Post
-
-
-class Periodic(object):
-    """ A periodic task running in threading.Timers """
-    # https://stackoverflow.com/a/18906292
-
-    def __init__(self, interval, function, *args, **kwargs):
-        self._lock = Lock()
-        self._timer = None
-        self.function = function
-        self.interval = interval
-        self.args = args
-        self.kwargs = kwargs
-        self._stopped = True
-        if kwargs.pop('autostart', True):
-            self.start()
-
-    def start(self, from_run=False):
-        self._lock.acquire()
-        if from_run or self._stopped:
-            self._stopped = False
-            self._timer = Timer(self.interval, self._run)
-            self._timer.start()
-            self._lock.release()
-
-    def _run(self):
-        self.start(from_run=True)
-        self.function(*self.args, **self.kwargs)
-
-    def stop(self):
-        self._lock.acquire()
-        self._stopped = True
-        self._timer.cancel()
-        self._lock.release()
 
 
 def get_video_metadata(video_id, youtube):
--- doxapp/utils.py
+++ doxapp/utils.py
@@ -1,5 +1,42 @@
+from threading import Timer, Lock
+
+
 def dump_datetime(value):
     """ Deserialize datetime object into string form for JSON processing. """
     if value is None:
         return None
     return [value.strftime("%Y-%m-%d"), value.strftime("%H:%M:%S")]
+
+
+class Periodic(object):
+    """ A periodic task running in threading.Timers """
+    # https://stackoverflow.com/a/18906292
+
+    def __init__(self, interval, function, *args, **kwargs):
+        self._lock = Lock()
+        self._timer = None
+        self.function = function
+        self.interval = interval
+        self.args = args
+        self.kwargs = kwargs
+        self._stopped = True
+        if kwargs.pop('autostart', True):
+            self.start()
+
+    def start(self, from_run=False):
+        self._lock.acquire()
+        if from_run or self._stopped:
+            self._stopped = False
+            self._timer = Timer(self.interval, self._run)
+            self._timer.start()
+            self._lock.release()
+
+    def _run(self):
+        self.start(from_run=True)
+        self.function(*self.args, **self.kwargs)
+
+    def stop(self):
+        self._lock.acquire()
+        self._stopped = True
+        self._timer.cancel()
+        self._lock.release()
