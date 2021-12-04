ede9a18404fa5f09a2a143eea20820f265a6813e
vlatan <33163699+vlatan@users.noreply.github.com>
2021-12-04 16:09:29 +0100

    Move decorator

--- doxapp/utils.py
+++ doxapp/utils.py
@@ -1,4 +1,18 @@
 from threading import Timer, Lock
+from functools import wraps
+from flask import current_app, flash, redirect, url_for
+from flask_login import current_user
+
+
+def admin_required(func):
+    @wraps(func)
+    def only_admin(*args, **kwargs):
+        admin_email = current_app.config['ADMIN_EMAIL']
+        if current_user.email != admin_email:
+            flash('You need to be admin to access that page!', 'info')
+            return redirect(url_for('main.home'))
+        return func(*args, **kwargs)
+    return only_admin
 
 
 def dump_datetime(value):
