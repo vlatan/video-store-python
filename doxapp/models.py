60c4a14c39b24e6750a412d419ceb443cde70d60
vlatan <33163699+vlatan@users.noreply.github.com>
2021-12-05 16:08:50 +0100

    Add date_posted to Channel

--- doxapp/models.py
+++ doxapp/models.py
@@ -34,6 +34,7 @@ class Channel(db.Model):
     title = db.Column(db.String(256), nullable=False)
     thumbnails = db.Column(db.PickleType, nullable=False)
     description = db.Column(db.Text, nullable=False)
+    date_posted = db.Column(db.DateTime, default=datetime.utcnow)
     posts = db.relationship('Post', backref='channel', lazy=True)
     user_id = db.Column(db.Integer, db.ForeignKey(User.id))
 
