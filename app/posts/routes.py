5de6a87372b34a02aa293b9f23660913554b8d9d
vlatan <33163699+vlatan@users.noreply.github.com>
2021-12-31 17:11:56 +0100

    Implement delete video option

--- app/posts/routes.py
+++ app/posts/routes.py
@@ -123,9 +123,7 @@ def playlists():
 @admin_required
 def delete_post(post_id):
     post = Post.query.get_or_404(post_id)
-    if post.author != current_user:
-        abort(403)
     db.session.delete(post)
     db.session.commit()
-    flash('Your post has been deleted', 'success')
+    flash('The video has been deleted', 'success')
     return redirect(url_for('main.home'))
