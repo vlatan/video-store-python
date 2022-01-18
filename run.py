3ada6d650c97dcbcb8c708d20b0c67e6aa60025d
vlatan <33163699+vlatan@users.noreply.github.com>
2022-01-18 18:41:59 +0100

    Enable HTTPS

--- run.py
+++ run.py
@@ -3,4 +3,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run('localhost', debug=True)
+    app.run('localhost', ssl_context=(
+        "certs/cert.pem", "certs/key.pem"), debug=True)
