b028139fb0d88983675ef192321519667e7c9d29
vlatan <33163699+vlatan@users.noreply.github.com>
2022-01-21 17:46:59 +0100

    Manually build Facebook login flow

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,3 +1,6 @@
+import os
+import hashlib
+import requests
 import google_auth_oauthlib.flow
 from google.oauth2 import id_token
 from google.auth.transport import requests as google_requests
@@ -10,75 +13,13 @@ from app.models import User
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/fb_test')
-def fb_test():
-    fb_app_id = current_app.config['FB_CLIENT_ID']
-    return render_template('fb_test.html', fb_app_id=fb_app_id)
-
-
-@auth.route('/authorize/onetap', methods=['POST'])
-def onetap():
-    if current_user.is_authenticated:
-        return redirect(request.referrer)
-
-    csrf_token_cookie = request.cookies.get('g_csrf_token')
-    if not csrf_token_cookie:
-        # No CSRF token in Cookie.
-        return failed_login()
-    csrf_token_body = request.form.get('g_csrf_token')
-    if not csrf_token_body:
-        # No CSRF token in post body.
-        return failed_login()
-    if csrf_token_cookie != csrf_token_body:
-        # Failed to verify double submit cookie.
-        return failed_login()
-
-    try:
-        # verify the integrity of the ID token and get the user info
-        token = request.form.get('credential')
-        CLIENT_ID = current_app.config['GOOGLE_OAUTH_CLIENT_ID']
-        # https://google-auth.readthedocs.io/en/stable/reference/google.oauth2.id_token.html#google.oauth2.id_token.verify_oauth2_token
-        user_info = id_token.verify_oauth2_token(
-            token, google_requests.Request(), CLIENT_ID)
-        # check if there's user ID, if not it will raise ValueError
-        openid = user_info['sub']
-    except ValueError:
-        # Invalid token
-        return failed_login()
-
-    user = get_user_ready(openid, user_info)
-    # begin user session by logging the user in
-    login_user(user, remember=True)
-    return redirect(request.referrer)
-
-
-@auth.route('/authorize/facebook', methods=['POST'])
-def facebook():
-    if current_user.is_authenticated:
-        return redirect(request.referrer)
-
-    if not (data := request.form):
-        return failed_login()
-
-    for item in data.items():
-        print(item)
-
-    print(request.referrer)
-
-    # grab first user for tesing
-    user = User.query.first()
-    # begin user session by logging the user in
-    login_user(user, remember=True)
-    return redirect(request.referrer)
-
-
-@auth.route('/authorize')
-def authorize():
+@auth.route('/authorize/google')
+def google():
     # https://developers.google.com/identity/protocols/oauth2/web-server
-    CLIENT_CONFIG = current_app.config['GOOGLE_OAUTH_CLIENT_CONFIG']
-    CLIENT_ID = current_app.config['GOOGLE_OAUTH_CLIENT_ID']
-    REDIRECT_URL = url_for('auth.authorize', _external=True)
-    SCOPES = current_app.config['GOOGLE_OAUTH_SCOPES']
+    CLIENT_CONFIG = current_app.config['GOOGLE_CLIENT_CONFIG']
+    CLIENT_ID = current_app.config['GOOGLE_CLIENT_ID']
+    REDIRECT_URL = url_for('auth.google', _external=True)
+    SCOPES = current_app.config['GOOGLE_SCOPES']
 
     # if the request DOESN'T have 'code' argument in the URL
     if 'code' not in request.args:
@@ -129,6 +70,117 @@ def authorize():
 
     # store revoke token in session in case the user want's to revoke access
     session['revoke_token'] = credentials.token
+
+    return render_template('close_oauth.html')
+
+
+@auth.route('/authorize/onetap', methods=['POST'])
+def onetap():
+    if current_user.is_authenticated:
+        return redirect(request.referrer)
+
+    csrf_token_cookie = request.cookies.get('g_csrf_token')
+    if not csrf_token_cookie:
+        # No CSRF token in Cookie.
+        return failed_login()
+    csrf_token_body = request.form.get('g_csrf_token')
+    if not csrf_token_body:
+        # No CSRF token in post body.
+        return failed_login()
+    if csrf_token_cookie != csrf_token_body:
+        # Failed to verify double submit cookie.
+        return failed_login()
+
+    try:
+        # verify the integrity of the ID token and get the user info
+        token = request.form.get('credential')
+        CLIENT_ID = current_app.config['GOOGLE_OAUTH_CLIENT_ID']
+        # https://google-auth.readthedocs.io/en/stable/reference/google.oauth2.id_token.html#google.oauth2.id_token.verify_oauth2_token
+        user_info = id_token.verify_oauth2_token(
+            token, google_requests.Request(), CLIENT_ID)
+        # check if there's user ID, if not it will raise ValueError
+        openid = user_info['sub']
+    except ValueError:
+        # Invalid token
+        return failed_login()
+
+    user = get_user_ready(openid, user_info)
+    # begin user session by logging the user in
+    login_user(user, remember=True)
+    return redirect(request.referrer)
+
+
+@auth.route('/authorize/facebook')
+def facebook():
+    # https://developers.facebook.com/docs/facebook-login/manually-build-a-login-flow#login
+
+    CLIENT_ID = current_app.config['FB_CLIENT_ID']
+    REDIRECT_URI = url_for('auth.facebook', _external=True)
+
+    # check if this view has 'code' argument in it
+    if not (CODE := request.args.get('code')):
+
+        # do not proceed if user is already logged in
+        if current_user.is_authenticated:
+            return redirect(request.referrer)
+
+        DIALOG_ENDPOINT = 'https://www.facebook.com/v12.0/dialog/oauth'
+        # create an anti-forgery unique token
+        STATE = hashlib.sha256(os.urandom(1024)).hexdigest()
+        # put it in a session
+        session['state'] = STATE
+        # construct the dialog url
+        dialog_uri = (f'{DIALOG_ENDPOINT}?response_type=code&'
+                      f'client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}&'
+                      f'state={STATE}&auth_type=rerequest&scope=email')
+        # redirect to the dialog uri
+        return redirect(dialog_uri)
+
+    # check if the anti-forgery unique session token is valid
+    if request.args.get('state') != session['state']:
+        # failed to complete the process
+        return render_template('close_oauth.html')
+
+    try:
+        # exchange the code for access token
+        ACCESS_TOKEN_ENDPOINT = 'https://graph.facebook.com/v12.0/oauth/access_token'
+        CLIENT_SECRET = current_app.config['FB_CLIENT_SECRET']
+        access_token_uri = (f'{ACCESS_TOKEN_ENDPOINT}?'
+                            f'client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}&'
+                            f'client_secret={CLIENT_SECRET}&code={CODE}')
+        # get response from the access_token_uri
+        credentials = requests.get(access_token_uri).json()
+
+        if not (ACCESS_TOKEN := credentials.get('access_token')):
+            # failed to complete the process
+            raise KeyError
+
+        # verify the access token we got
+        INSPECT_TOKEN_ENDPOINT = 'https://graph.facebook.com/debug_token'
+        inspect_token_uri = (f'{INSPECT_TOKEN_ENDPOINT}?input_token={ACCESS_TOKEN}&'
+                             f'access_token={CLIENT_ID}|{CLIENT_SECRET}')
+        # get response from the inspect_token_uri
+        data = requests.get(inspect_token_uri).json().get('data')
+
+        if not (data and data.get('is_valid') and (USER_ID := (data.get('user_id')))):
+            # failed to complete the process
+            raise KeyError
+
+        # get user info
+        GRAPH_ENDPOINT = 'https://graph.facebook.com/v12.0/'
+        user_info_uri = (f'{GRAPH_ENDPOINT}{USER_ID}?access_token={ACCESS_TOKEN}&'
+                         'fields=id,first_name,picture,email')
+        # get response from the user_info_uri
+        user_info = requests.get(user_info_uri).json()
+
+        if user_info:
+            # grab the first user for testing
+            user = User.query.first()
+            # begin user session by logging the user in
+            login_user(user, remember=True)
+    except Exception:
+        # failed to complete the process
+        return render_template('close_oauth.html')
 
     return render_template('close_oauth.html')
 
--- app/config.py
+++ app/config.py
@@ -4,31 +4,45 @@ from dotenv import load_dotenv
 
 
 class Config:
+    # load the enviroment variables
     load_dotenv()
+
+    # Flask app secret key
     SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    # Admin Google opend
     ADMIN_OPENID = os.environ.get('ADMIN_OPENID')
+
+    # SQlAlchemy stuff
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SCOPPED_SESSION_DB_URI = os.environ.get('SCOPPED_SESSION_DB_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Other stuff
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
     POSTS_PER_PAGE = int(os.environ.get('POSTS_PER_PAGE'))
     NUM_RELATED_POSTS = int(os.environ.get('NUM_RELATED_POSTS'))
+
+    # Facebook authentication
     FB_CLIENT_ID = os.environ.get('FB_CLIENT_ID')
+    FB_CLIENT_SECRET = os.environ.get('FB_CLIENT_SECRET')
+
+    # Google/Youtube authentication
     YOUTUBE_API_KEY = os.environ.get('YOUTUBE_API_KEY')
-    GOOGLE_OAUTH_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID')
-    GOOGLE_OAUTH_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET')
-    GOOGLE_OAUTH_SCOPES = json.loads(os.environ.get('GOOGLE_SCOPES'))
-    GOOGLE_OAUTH_CLIENT_CONFIG = {
+    GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID')
+    GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET')
+    GOOGLE_SCOPES = json.loads(os.environ.get('GOOGLE_SCOPES'))
+    GOOGLE_CLIENT_CONFIG = {
         "web": {
-            "client_id": GOOGLE_OAUTH_CLIENT_ID,
+            "client_id": GOOGLE_CLIENT_ID,
             "project_id": "doxder",
             "auth_uri": "https://accounts.google.com/o/oauth2/auth",
             "token_uri": "https://oauth2.googleapis.com/token",
             "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-            "client_secret": GOOGLE_OAUTH_CLIENT_SECRET,
+            "client_secret": GOOGLE_CLIENT_SECRET,
             "redirect_uris": [
-                "http://localhost:5000/authorize",
-                "https://localhost:5000/authorize"
+                "http://localhost:5000/authorize/google",
+                "https://localhost:5000/authorize/google"
             ],
             "javascript_origins": [
                 "http://localhost",
