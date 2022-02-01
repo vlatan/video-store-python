f0da74241503c976892a2b4ac69f5b1507460f83
vlatan <33163699+vlatan@users.noreply.github.com>
2022-02-01 12:44:22 +0100

    Finish up mobile search box

--- app/static/js/shared.js
+++ app/static/js/shared.js
@@ -1,20 +1,34 @@
-
-// modal
-var openModal = document.getElementById('openModal');
-var noModal = [document.getElementById('modal'),
-document.getElementById('closeModal'),
-document.getElementById('cancelModal')]
-
-window.onclick = function (event) {
-    if (event.target == openModal) {
+window.addEventListener('click', function (event) {
+    // Modal
+    var modal = document.getElementById('modal');
+    var closeModal = [modal, document.getElementById('closeModal'),
+        document.getElementById('cancelModal')]
+    if (event.target.closest('#openModal')) {
         modal.style.display = 'flex'
-    }
-    else if (noModal.includes(event.target)) {
+    } else if (closeModal.includes(event.target)) {
         modal.style.display = "none";
     }
-}
 
-// get login state
+    // Dropdown menu
+    var dropContent = document.querySelector('.dropdown-content');
+    if (event.target.closest('.username')) {
+        dropContent.classList.add('show-dropdown');
+    } else if (!event.target.closest('.dropdown')) {
+        dropContent.classList.remove('show-dropdown');
+    }
+
+    // Mobile search form
+    var searchForm = document.querySelector('.search-form');
+    if (event.target.closest('.search-button-mobile')) {
+        searchForm.classList.remove('hide');
+        searchForm.classList.add('search-form-mobile');
+    } else if (!event.target.closest('.search-form')) {
+        searchForm.classList.add('hide');
+        searchForm.classList.remove('search-form-mobile');
+    }
+});
+
+// save login state for parent window
 var loginState = localStorage.getItem('LoggedIn');
 if (loginState) {
     var logIn = document.getElementById('logInMessage');
