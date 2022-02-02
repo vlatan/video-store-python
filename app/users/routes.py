21021b315a5ea100144f349305b2768007db78c9
vlatan <33163699+vlatan@users.noreply.github.com>
2022-02-02 11:26:50 +0100

    Rename Favorites

--- app/users/routes.py
+++ app/users/routes.py
@@ -7,24 +7,6 @@ from app.users.forms import UpdateAccountForm
 users = Blueprint('users', __name__)
 
 
-@users.route('/account', methods=['GET', 'POST'])
-@login_required
-def account():
-    form = UpdateAccountForm()
-    if form.validate_on_submit():
-        current_user.name = form.username.data
-        current_user.email = form.email.data
-        db.session.commit()
-        flash('Your account has been updated!', 'success')
-        return redirect(url_for('users.account'))
-    elif request.method == 'GET':
-        form.username.data = current_user.name
-        form.email.data = current_user.email
-    image_file = current_user.picture
-    return render_template('account.html', title='Account',
-                           image_file=image_file, form=form)
-
-
 @users.route('/favorites')
 @login_required
 def favorites():
