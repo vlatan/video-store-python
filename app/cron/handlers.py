58fb9cafc1f1ef76089e0b7d5d178ad804a3d210
vlatan <33163699+vlatan@users.noreply.github.com>
2022-02-20 18:23:41 +0100

    Refactor post new videos

--- app/cron/handlers.py
+++ app/cron/handlers.py
@@ -1,9 +1,7 @@
+import time
 import random
-import atexit
-import logging
-from pytz import utc
-from datetime import datetime, timedelta
-from flask import current_app
+from datetime import datetime
+from flask import current_app, Blueprint
 from wtforms.validators import ValidationError
 from app import db
 from app.models import Post, Playlist
@@ -11,7 +9,9 @@ from app.cron.helpers import get_playlist_videos
 from app.posts.helpers import validate_video
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-from apscheduler.schedulers.background import BackgroundScheduler
+from sqlalchemy import func
+
+cron = Blueprint('cron', __name__)
 
 
 def post_new_videos(app):
@@ -20,54 +20,59 @@ def post_new_videos(app):
         PER_PAGE = current_app.config['NUM_RELATED_POSTS']
         # get all playlists from db
         playlists, all_videos = Playlist.query.all(), []
-        # construct youtube API service
-        with build('youtube', 'v3', developerKey=API_KEY) as youtube:
-            # loop through the playlists
-            for playlist in playlists:
-                # get playlist videos from YT
-                playlist_videos = get_playlist_videos(
-                    playlist.playlist_id, youtube)
-                # loop through the videos in this playlist
-                for video in playlist_videos:
-                    # add relationship with this playlist to the video metadata
-                    video['playlist'] = playlist
-                # add this batch of videos to the total list of videos
-                all_videos += playlist_videos
 
-        # shuffle videos so the don't get posted uniformly
-        random.shuffle(all_videos)
+    # construct youtube API service
+    with build('youtube', 'v3', developerKey=API_KEY,
+               cache_discovery=False) as youtube:
+        # loop through the playlists
+        for playlist in playlists:
+            # get playlist videos from YT
+            playlist_videos = get_playlist_videos(
+                playlist.playlist_id, youtube)
+            # loop through the videos in this playlist
+            for video in playlist_videos:
+                # add relationship with this playlist to the video metadata
+                video['playlist'] = playlist
+            # add this batch of videos to the total list of videos
+            all_videos += playlist_videos
 
-        # loop through total number of videos
-        for video in all_videos:
-            # if video is already posted (via webform as a single video submit)
-            if (posted := Post.query.filter_by(video_id=video['video_id']).first()):
-                # if it doesn't have playlist id
-                if not posted.playlist_id:
-                    # add playlist id
-                    posted.playlist_id = video['playlist_id']
-                    # asscoiate with existing playlist in our db
-                    posted.playlist = video['playlist']
-                    # commit
-                    db.session.commit()
-            else:
-                # create object from Model
-                post = Post(**video)
-                # search for related videos using the post title
-                if (related_posts := Post.search(post.title, 1, PER_PAGE)[0].all()):
-                    post.related_posts = related_posts
+    # shuffle videos so the don't get posted uniformly
+    random.shuffle(all_videos)
 
-                # add post to database
-                db.session.add(post)
-                db.session.commit()
+    # loop through total number of videos
+    for video in all_videos:
+        with app.app_context():
+            # do the work in db context menager (autocommit)
+            # to ensure db is left in a healthy state if exception is raised
+            # transactions commited ор rolled back and session closed
+            with db.session.begin():
+                # if video is already posted
+                if (posted := Post.query.filter_by(video_id=video['video_id']).first()):
+                    # if it doesn't have playlist id
+                    if not posted.playlist_id:
+                        # add playlist id
+                        posted.playlist_id = video['playlist_id']
+                        # asscoiate with existing playlist in our db
+                        posted.playlist = video['playlist']
+                else:
+                    # search for related videos using the post title
+                    related_posts = Post.search(
+                        video['title'], 1, PER_PAGE)[0].all()
+                    # create object from Model
+                    post = Post(**video, related_posts=related_posts)
+                    # add post to database
+                    db.session.add(post)
+        time.sleep(5)
 
 
 def revalidate_video(post, api_key, per_page):
-    with build('youtube', 'v3', developerKey=api_key) as youtube:
+    with build('youtube', 'v3', developerKey=api_key,
+               cache_discovery=False) as youtube:
         try:
             scope = {'id': post.video_id,
                      'part': ['status', 'snippet', 'contentDetails']}
             req = youtube.videos().list(**scope)
-            # this will raise IndexError if ['items'] is empty list
+            # this will raise IndexError if ['items'] is empty list``
             # which means the video does not exist
             res = req.execute()['items'][0]
             # this will raise ValidationError if video's invalid
@@ -95,34 +100,102 @@ def revalidate_video(post, api_key, per_page):
         db.session.commit()
 
 
+# def revalidate_existing_videos(app):
+#     with app.app_context():
+#         API_KEY = current_app.config['YOUTUBE_API_KEY']
+#         PER_PAGE = current_app.config['NUM_RELATED_POSTS']
+#         time_flag = datetime.utcnow() - timedelta(days=3)
+#         posts = Post.query.filter(Post.last_checked < time_flag)
+#         for post in posts:
+#             revalidate_video(post, API_KEY, PER_PAGE)
+
 def revalidate_existing_videos(app):
     with app.app_context():
+        print('Started revalidating videos.')
         API_KEY = current_app.config['YOUTUBE_API_KEY']
         PER_PAGE = current_app.config['NUM_RELATED_POSTS']
-        time_flag = datetime.utcnow() - timedelta(days=3)
-        posts = Post.query.filter(Post.last_checked < time_flag)
-        for post in posts:
-            revalidate_video(post, API_KEY, PER_PAGE)
+        posts = Post.query.order_by(Post.id.desc()).all()
+        print(posts[:10])
+        part = ['status', 'snippet', 'contentDetails']
+
+        with build('youtube', 'v3', developerKey=API_KEY, cache_discovery=False) as youtube:
+            # you can get max 50 videos per call from YouTube API
+            for i in range(0, len(posts), 50):
+                track_posts = {post.video_id: post for post in posts[i:i+50]}
+                try:
+                    req = youtube.videos().list(id=list(track_posts.keys()), part=part)
+                    items = req.execute()['items']
+                    print(f'{len(track_posts)} items fetched from YouTube API')
+                except HttpError:
+                    # we couldn't connect to YouTube API,
+                    # so we can't evaluate this entire chunk
+                    print('Couldn\'t connect to YouTube API')
+                    continue
+
+                print('Looping through the items.')
+                for item in items:
+                    post = track_posts.get(item['id'])
+                    print(f'Processing post {post.id}')
+                    try:
+                        # this will raise ValidationError if not valid
+                        validate_video(item)
+                        print(f'Video {post.id} is VALID.')
+
+                        # get related posts by searching the index,
+                        # using the title of this post
+                        related_posts = Post.search(
+                            post.title, 1, PER_PAGE + 1)[0].all()[1:]
+
+                        if related_posts:
+                            if post.related_posts != related_posts:
+                                post.related_posts = related_posts
+                                db.session.commit()
+                                print(
+                                    f'Related posts for post {post.id} updated.')
+                        else:
+                            post.related_posts = Post.query.order_by(
+                                func.random()).limit(PER_PAGE).all()
+                            db.session.commit()
+                            print(
+                                f'Related posts for post {post.id} updated (random).')
+
+                        print('-' * 30)
+
+                    except ValidationError:
+                        # delete invalid video
+                        db.session.delete(post)
+                        db.session.commit()
+                        print(f'Video {post.id} is INVALID and deleted.')
+                        print('-' * 30)
+
+                    # remove video from tracked posts
+                    track_posts.pop(post.video_id)
+                    time.sleep(0.5)
+
+                if items:
+                    # posts still remaining in track_posts were not
+                    # present at YouTube at all, therefore delete from db
+                    for post in track_posts.values():
+                        db.session.delete(post)
+                        db.session.commit()
+                        print(f'Video {post.id} is MISSING and deleted.')
+                        time.sleep(0.5)
 
 
-def init_scheduler():
+def init_scheduler_jobs():
     # https://stackoverflow.com/a/38501328
     # https://flask.palletsprojects.com/en/0.12.x/reqcontext/#notes-on-proxies
 
-    # configure scheduler
-    scheduler = BackgroundScheduler(timezone=utc)
-    scheduler.start()
-    logging.getLogger('apscheduler').setLevel(logging.WARNING)
-    atexit.register(lambda: scheduler.shutdown(wait=False))
-
     # add background job that posts new videos once a day
-    scheduler.add_job(func=post_new_videos,
-                      args=[current_app._get_current_object()],
-                      trigger='interval', days=1,
-                      id='post', replace_existing=True)
+    # current_app.scheduler.add_job(func=post_new_videos,
+    #                               args=[current_app._get_current_object()],
+    #                               trigger='cron', minute=59,
+    #                               id='post', replace_existing=True)
 
     # add background job that revalidates all eligible videos every two days
-    scheduler.add_job(func=revalidate_existing_videos,
-                      args=[current_app._get_current_object()],
-                      trigger='interval', days=2,
-                      id='revalidate', replace_existing=True)
+    # current_app.scheduler.add_job(func=revalidate_existing_videos,
+    #                               args=[current_app._get_current_object()],
+    #                               trigger='cron', minute=12,
+    #                               id='revalidate', replace_existing=True)
+
+    pass
