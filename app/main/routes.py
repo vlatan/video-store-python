f67c2240fc02a8c44769ae0b196b74cd64847c2e
vlatan <33163699+vlatan@users.noreply.github.com>
2022-02-21 13:00:33 +0100

    Use class methods to fetch posts

--- app/main/routes.py
+++ app/main/routes.py
@@ -3,6 +3,7 @@ import time
 from flask import render_template, request, current_app
 from flask import Blueprint, jsonify, make_response
 from app.models import Post, PostLike
+from app import cache
 from sqlalchemy import func
 
 main = Blueprint('main', __name__)
@@ -29,23 +30,12 @@ def home():
     # if frontend_data get page number, else 1
     page = frontend_data.get('page') if frontend_data else 1
 
-    if request.args.get('order_by') == 'likes':
-        # query posts by likes (outerjoin)
-        # https://stackoverflow.com/q/63889938
-        posts_query = Post.query.outerjoin(PostLike).group_by(
-            Post.id).order_by(func.count().desc())
-    else:
-        # query posts in descending order
-        posts_query = Post.query.order_by(Post.id.desc())
-
-    posts = posts_query.paginate(page, per_page, False).items
+    posts = Post.get_posts_by_likes(page, per_page) if request.args.get(
+        'order_by') == 'likes' else Post.get_posts(page, per_page)
 
     if request.method == 'POST':
-        # if there are subsequent pages send posts as JSON object
-        posts = jsonify([post.serialize for post in posts])
-        # Simulate delay
         time.sleep(0.4)
-        return make_response(posts, 200)
+        return make_response(jsonify(posts), 200)
 
     # render template on the first view (GET method)
     return render_template('content.html', posts=posts,
