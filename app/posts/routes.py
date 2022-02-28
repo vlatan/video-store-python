3ea373d42398de89ca796f39f1ecf6a82f7115c2
vlatan <33163699+vlatan@users.noreply.github.com>
2022-02-28 14:18:46 +0100

    Get related posts from cache

--- app/posts/routes.py
+++ app/posts/routes.py
@@ -1,11 +1,12 @@
 from flask import render_template, url_for, flash
 from flask import redirect, Blueprint, current_app, make_response
 from flask_login import current_user, login_required
-from app import db
+from app import db, cache
 from app.helpers import admin_required
 from app.models import Post, Playlist
 from app.posts.forms import PostForm, PlaylistForm
 from app.posts.helpers import convertDuration
+from sqlalchemy import func
 
 posts = Blueprint('posts', __name__)
 
@@ -24,8 +25,12 @@ def post(video_id):
     if not num_likes:
         likes = 'Like'
 
+    PER_PAGE = current_app.config['NUM_RELATED_POSTS']
+    related_posts = Post.get_related_posts(post.title, PER_PAGE)
+
     return render_template('post.html', post=post, thumb=thumb['url'],
-                           duration=duration.human, likes=likes, title=post.title)
+                           duration=duration.human, likes=likes,
+                           title=post.title, related_posts=related_posts)
 
 
 @posts.route('/video/new', methods=['GET', 'POST'])
@@ -38,12 +43,9 @@ def new_post():
     if form.validate_on_submit():
         per_page = current_app.config['NUM_RELATED_POSTS']
         post_title = form.content.data['title']
-        related_posts = Post.search(post_title, 1, per_page)[0].all()
 
         # form.content.data is a dict, just unpack to transform into kwargs
-        post = Post(**form.content.data,
-                    related_posts=related_posts,
-                    author=current_user)
+        post = Post(**form.content.data, author=current_user)
 
         # add post to database
         db.session.add(post)
