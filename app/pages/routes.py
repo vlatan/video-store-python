31a20fe2d8be59d2eeda7920b88a9abc03c9c186
vlatan <33163699+vlatan@users.noreply.github.com>
2022-03-07 18:45:11 +0100

    Move from html to posted ia form pages

--- app/pages/routes.py
+++ app/pages/routes.py
@@ -1,12 +1,19 @@
 
-from flask import render_template, url_for, flash
-from flask import redirect, Blueprint, current_app, make_response
+import markdown
+from flask import redirect, render_template, flash, url_for, Blueprint
 from app import db
 from app.models import Page
 from app.pages.forms import PageForm
 from app.helpers import admin_required
 
 pages = Blueprint('pages', __name__)
+
+
+@pages.route('/page/<int:id>/')
+def page(id):
+    page = Page.query.get_or_404(id)
+    return render_template('page.html', title=page.title,
+                           page_content=markdown.markdown(page.content))
 
 
 @pages.route('/page/new', methods=['GET', 'POST'])
@@ -16,28 +23,23 @@ def new_page():
     if form.validate_on_submit():
         page = Page(title=form.title.data, content=form.content.data)
         db.session.add(page)
+        db.session.commit()
         flash('Your page has been created!', 'success')
-        return render_template('page.html', page=page)
+        return redirect(url_for('pages.page', id=page.id))
     return render_template('form.html', title='Add New Page',
                            form=form, legend='New Page')
 
 
-@pages.route('/page/<int:page_id>/')
-def page(page_id):
-    page = Page.query.get_or_404(page_id)
-    return render_template('page.html', page=page)
-
-
-@pages.route('/page/<int:page_id>/edit', methods=['GET', 'POST'])
+@pages.route('/page/<int:id>/edit', methods=['GET', 'POST'])
 @admin_required
-def edit_page(page_id):
-    page = Page.query.get_or_404(page_id)
+def edit_page(id):
+    page = Page.query.get_or_404(id)
     form = PageForm()
-    form.title.data, form.content.data = page.title, page.text
     if form.validate_on_submit():
-        page = Page(title=form.title.data, content=form.content.data)
-        db.session.add(page)
+        page.title, page.content = form.title.data, form.content.data
+        db.session.commit()
         flash('Your page has been updated!', 'success')
-        return render_template('page.html', page=page)
+        return redirect(url_for('pages.page', id=page.id))
+    form.title.data, form.content.data = page.title, page.content
     return render_template('form.html', title='Edit This Page',
                            form=form, legend='Edit Page')
