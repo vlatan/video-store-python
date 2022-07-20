772cdd704ef62571f1152686e6005c76fba3c3eb
vlatan <33163699+vlatan@users.noreply.github.com>
2022-07-20 17:11:25 +0200

    Black autoformat and guard clause

--- app/auth/helpers.py
+++ app/auth/helpers.py
@@ -4,29 +4,30 @@ from flask import flash, redirect, request
 
 
 def failed_login():
-    flash('Sorry, there was a problem signing in to your account.', 'info')
+    flash("Sorry, there was a problem signing in to your account.", "info")
     return redirect(request.referrer)
 
 
 def get_user_ready(user_info):
-    if (facebook_id := user_info.get('facebook_id')):
+    if facebook_id := user_info.get("facebook_id"):
         user = User.query.filter_by(facebook_id=facebook_id).first()
     else:
-        user = User.query.filter_by(google_id=user_info['google_id']).first()
+        user = User.query.filter_by(google_id=user_info["google_id"]).first()
 
     # if this user does not exist, create it
     if not user:
         user = User(**user_info)
         db.session.add(user)
+        db.session.commit()
+        return user
 
     # otherwise update mutable info for this user if changed
-    else:
-        if user.email != user_info['email']:
-            user.email = user_info['email']
-        if user.name != user_info['name']:
-            user.name = user_info['name']
-        if user.picture != user_info['picture']:
-            user.picture = user_info['picture']
+    if user.email != user_info["email"]:
+        user.email = user_info["email"]
+    if user.name != user_info["name"]:
+        user.name = user_info["name"]
+    if user.picture != user_info["picture"]:
+        user.picture = user_info["picture"]
 
     db.session.commit()
     return user
