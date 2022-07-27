6f696981355490db14eed2c907fb4406ac54d7ec
vlatan <33163699+vlatan@users.noreply.github.com>
2022-07-27 22:01:33 +0200

    Use const

--- app/static/js/play.js
+++ app/static/js/play.js
@@ -1,11 +1,10 @@
-var v = document.querySelector('.player-content');
-v.onclick = playerIframe;
-
-function playerIframe() {
-    var e = document.createElement("iframe");
+const v = document.querySelector('.player-content');
+const playerIframe = function () {
+    const e = document.createElement("iframe");
     e.setAttribute("src", "https://www.youtube-nocookie.com/embed/" + v.id + "?iv_load_policy=3&cc_load_policy=1&autoplay=1");
     e.setAttribute("frameborder", "0");
     e.setAttribute("allow", "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture");
     e.setAttribute("allowfullscreen", "");
     this.parentNode.replaceChild(e, this);
 }
+v.onclick = playerIframe;
