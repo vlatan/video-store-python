cee15ece917831d44d497d05510dbcc60ec0a70d
vlatan <33163699+vlatan@users.noreply.github.com>
2022-07-27 22:03:27 +0200

    Use const

--- app/static/js/pop.js
+++ app/static/js/pop.js
@@ -1,6 +1,6 @@
-function popupWindow(url) {
-    var w = window.top.innerWidth * 0.4;
-    var h = window.top.innerHeight * 0.9;
+const popupWindow = url => {
+    const w = window.top.innerWidth * 0.4;
+    const h = window.top.innerHeight * 0.9;
     const y = window.top.outerHeight / 2 + window.top.screenY - (h / 2);
     const x = window.top.outerWidth / 2 + window.top.screenX - (w / 2);
     return window.open(url, '_blank', `width=${w}, height=${h}, top=${y}, left=${x}`);
