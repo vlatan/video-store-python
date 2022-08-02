4d576727e5df5eb76f5f0d2cd609786b425e6020
vlatan <33163699+vlatan@users.noreply.github.com>
2022-08-02 22:46:05 +0200

    Add config

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,22 +1,37 @@
 import pytest
 from app.models import User
-
-test_user_info = {
-    "name": "Test User",
-    "email": "test@email.com",
-    "picture": "https://path.to.picture.com",
-}
+from app import create_app
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture()
+def app():
+    app = create_app()
+    app.config.update({"TESTING": True})
+    yield app
+
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+
+def user_info():
+    return {
+        "name": "Test User",
+        "email": "test@email.com",
+        "picture": "https://path.to.picture.com",
+    }
+
+
+@pytest.fixture()
 def new_google_user():
-    google_info = test_user_info.copy()
+    google_info = user_info().copy()
     google_info["google_id"] = "12345677890"
     return User(**google_info)
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture()
 def new_facebook_user():
-    facebook_info = test_user_info.copy()
+    facebook_info = user_info().copy()
     facebook_info["facebook_id"] = "0987654321"
     return User(**facebook_info)
