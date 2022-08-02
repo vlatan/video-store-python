29517053c75ec3a0a959a5b858cc02866f77f948
vlatan <33163699+vlatan@users.noreply.github.com>
2022-08-02 22:46:47 +0200

    Assert new user

--- app/tests/unit/test_models.py
+++ app/tests/unit/test_models.py
@@ -1,15 +1,19 @@
 import hashlib
 
 
+def assert_new_user(user):
+    assert user.name == "Test User"
+    assert user.email == "test@email.com"
+    assert user.picture == "https://path.to.picture.com"
+
+
 def test_new_google_user(new_google_user):
     """
     GIVEN a User model
     WHEN a new User is created
     THEN check the name email, picture url, google id and analytics id
     """
-    assert new_google_user.name == "Test User"
-    assert new_google_user.email == "test@email.com"
-    assert new_google_user.picture == "https://path.to.picture.com"
+    assert_new_user(new_google_user)
     assert new_google_user.google_id == "12345677890"
     assert new_google_user.facebook_id == None
 
@@ -24,9 +28,7 @@ def test_new_facebook_user(new_facebook_user):
     WHEN a new User is created
     THEN check the name email, picture url, facebook id and analytics id
     """
-    assert new_facebook_user.name == "Test User"
-    assert new_facebook_user.email == "test@email.com"
-    assert new_facebook_user.picture == "https://path.to.picture.com"
+    assert_new_user(new_facebook_user)
     assert new_facebook_user.google_id == None
     assert new_facebook_user.facebook_id == "0987654321"
 
