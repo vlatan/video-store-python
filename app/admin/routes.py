2aa68501ed7c742555ab5609a37f91f0f8c35b9b
vlatan <33163699+vlatan@users.noreply.github.com>
2022-08-10 22:18:36 +0200

    Define admin route

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -1,3 +1,10 @@
 from flask import Blueprint, url_for, render_template
+from app.helpers import admin_required
 
 admin = Blueprint("admin", __name__)
+
+
+@admin.route("/admin/")
+@admin_required
+def admin():
+    pass
