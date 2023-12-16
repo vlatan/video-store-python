7b912085aae126bb10fbc28a734bd1edd50a6cb0
vlatan <33163699+vlatan@users.noreply.github.com>
2023-12-16 17:22:08 +0100

    Fix obscured func name

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -20,7 +20,7 @@ def error_404(error):
 
 
 @bp.app_errorhandler(405)
-def error_403(error):
+def error_405(error):
     heading = "You don't have permission to do that (405)"
     text = "Please check your account and try again."
     return render_template("error.html", title="405", heading=heading, text=text), 405
