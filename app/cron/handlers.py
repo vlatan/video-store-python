ce004370dcb3106766e0c8dd4379fbe1d9be7082
vlatan <33163699+vlatan@users.noreply.github.com>
2023-12-26 12:36:06 +0100

    Generate just one paragraph description

--- app/cron/handlers.py
+++ app/cron/handlers.py
@@ -192,7 +192,7 @@ def generate_description(title: str) -> str | None:
     """Generate description from a generative AI given a title."""
 
     generate_content = current_app.config["generate_content"]
-    prompt = f"Write a short text about: {title}."
+    prompt = f"Write one paragraph about: {title}."
 
     try:
         return generate_content(prompt).text
