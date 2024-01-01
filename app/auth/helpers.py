5ac0c729817ad0a22319aa92fb1b932e0a72a905
vlatan <33163699+vlatan@users.noreply.github.com>
2024-01-01 18:32:32 +0100

    Catch download error

--- app/auth/helpers.py
+++ app/auth/helpers.py
@@ -87,7 +87,8 @@ def get_user_ready(user_info):
         user.name = user_info["name"]
     if user.picture != user_info["picture"]:
         user.picture = user_info["picture"]
-        download_avatar(user)
+
+    download_avatar(user)
 
     db.session.commit()
     return user
@@ -103,14 +104,17 @@ def generate_hash(user):
 
 def download_avatar(user):
     """Download avatar to disc."""
-    if not user.picture:
-        return
+    try:
+        if not user.picture:
+            return
 
-    response = requests.get(user.picture)
-    if response.ok:
-        avatar_path = get_avatar_abs_path(user)
-        with open(avatar_path, "wb") as file:
-            file.write(response.content)
+        response = requests.get(user.picture)
+        if response.ok:
+            avatar_path = get_avatar_abs_path(user)
+            with open(avatar_path, "wb") as file:
+                file.write(response.content)
+    except Exception as e:
+        current_app.logger.warning(f"Unable to download avatar - {e}")
 
 
 def get_avatar_abs_path(user):
