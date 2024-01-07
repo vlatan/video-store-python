5591289080cd5b86beb110173088330197092192
vlatan <33163699+vlatan@users.noreply.github.com>
2024-01-07 20:33:24 +0100

    Categorize video on worker cron job

--- app/cron/handlers.py
+++ app/cron/handlers.py
@@ -9,9 +9,9 @@ from flask.ctx import AppContext
 from flask import Flask, current_app
 
 from app import db
-from app.models import Post, Playlist
 from app.helpers import youtube_build
 from app.posts.helpers import validate_video
+from app.models import Post, Playlist, Category
 from app.cron.helpers import get_playlist_videos
 from app.sources.helpers import validate_playlist
 
@@ -40,6 +40,7 @@ def get_youtube_videos() -> tuple[list[dict], bool]:
                 video["playlist"] = playlist
             # add this batch of videos to the total list of videos
             all_videos += playlist_videos
+            break
 
     # remove duplicates if any
     all_videos = list({v["video_id"]: v for v in all_videos}.values())
@@ -91,6 +92,12 @@ def process_videos() -> None:
     # get all VALID videos from our playlists from YouTube
     all_videos, complete = get_youtube_videos()
 
+    # get all possible categories from DB
+    categories = db.session.execute(db.select(Category)).scalars().all()
+    categories = {category.name: category for category in categories}
+    categories_string = ", ".join(categories)
+    categories_string.replace('"', "")
+
     current_app.logger.info(f"Processing {len(all_videos)} videos...")
     # loop through total number of videos
     for video in all_videos:
@@ -99,43 +106,55 @@ def process_videos() -> None:
 
         # if video is already posted
         if posted := Post.query.filter_by(video_id=video["video_id"]).first():
-            # if it doesn't match the playlist id
-            if posted.playlist_id != video["playlist_id"]:
-                # match playlist id
-                posted.playlist_id = video["playlist_id"]
-                # associate with existing playlist in our db
-                posted.playlist = video["playlist"]
+            try:
+                # if it doesn't match the playlist id
+                if posted.playlist_id != video["playlist_id"]:
+                    # match playlist id
+                    posted.playlist_id = video["playlist_id"]
+                    # associate with existing playlist in our db
+                    posted.playlist = video["playlist"]
+
+                # update similar_ids if there's a change
+                similar = Post.get_related_posts(posted.title, PER_PAGE)
+                if video_ids := [row["video_id"] for row in similar]:
+                    when = [(value, i) for i, value in enumerate(video_ids)]
+                    similar = (
+                        Post.query.filter(Post.video_id.in_(video_ids))
+                        .order_by(db.case(*when, value=Post.video_id))
+                        .limit(PER_PAGE)
+                        .all()
+                    )
+                    similar = [row.id for row in similar]
+
+                    if posted.similar != similar:
+                        posted.similar = similar
+
+                # if there is NO short description in DB generate one
+                if not posted.short_description:
+                    if short_desc := generate_description(posted.title):
+                        posted.short_description = short_desc
+
+                # if the video is not categorized, do it
+                if not posted.category:
+                    category = categorize(posted.title, categories_string)
+                    if category in categories:
+                        posted.category_id = categories[category].id
+                        posted.category = categories[category]
+            finally:
+                # If no pending changes are detected,
+                # then no SQL is emitted to the database
+                # https://docs.sqlalchemy.org/en/20/orm/session_basics.html#committing
                 db.session.commit()
-
-            # update similar_ids if there's a change
-            similar = Post.get_related_posts(posted.title, PER_PAGE)
-            if video_ids := [row["video_id"] for row in similar]:
-                when = [(value, i) for i, value in enumerate(video_ids)]
-                similar = (
-                    Post.query.filter(Post.video_id.in_(video_ids))
-                    .order_by(db.case(*when, value=Post.video_id))
-                    .limit(PER_PAGE)
-                    .all()
-                )
-                similar = [row.id for row in similar]
-
-                if posted.similar != similar:
-                    posted.similar = similar
-                    db.session.commit()
-
-            # if there is NO short description in DB generate one
-            if not posted.short_description:
-                if short_desc := generate_description(posted.title):
-                    posted.short_description = short_desc
-                    db.session.commit()
-
-            # TODO: Categorize the video using generative AI
-
         else:
+            # generate short description
             if short_desc := generate_description(video["title"]):
                 video["short_description"] = short_desc
 
-            # TODO: Categorize the video using generative AI
+            # categorize the video
+            category = categorize(video["title"], categories_string)
+            if category in categories:
+                video["category_id"] = categories[category].id
+                video["category"] = categories[category]
 
             # create object from Model
             post = Post(**video)
