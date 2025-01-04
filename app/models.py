3183c43234bbc2da4b1aa15e628a0e86eb212a06
vlatan <33163699+vlatan@users.noreply.github.com>
2025-01-04 14:45:47 +0100

    Do not memoize the get_related_posts

--- app/models.py
+++ app/models.py
@@ -249,7 +249,7 @@ class Post(Base, SearchableMixin):
         return [post.serialize for post in posts]
 
     @classmethod
-    @cache.memoize(86400)
+    # @cache.memoize(86400)
     def get_related_posts(cls, title: str, per_page: int):
         search_result = cls.search(title, 0, per_page + 1)
         search_result = [
